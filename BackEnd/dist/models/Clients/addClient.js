16f48b6d6c88caab6e93c511adef344f3d93c756
AdnenKhiari <adnenkhiari484@gmail.com>
2022-05-26 12:25:47 +0100

    removed dist

--- BackEnd/dist/models/Clients/addClient.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = `
-    INSERT INTO CLIENTS VALUES (:fullname,:address,:date_of_birth,:cin,:job,:gender,:state)
-`;
--- BackEnd/dist/models/Clients/getClientInfo.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GETCLIENTINFO_STATEMENT = void 0;
-exports.GETCLIENTINFO_STATEMENT = `
-    SELECT id,fullname,cin,gender FROM CLIENTS;
-`;
